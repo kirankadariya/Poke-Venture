371abb0cd26fb822bae5f3440ad00c6ac63021b4
Kiran Kadariya <kirankadariya@gmail.com>
2024-01-14 13:40:40 -0600

    update js file

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ const main = document.querySelector("main");
 const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
 async function getPokemon(){
-    const response =await fetch(url)
-    const data =await response.json();
-    console.log(data)
+    try {const response =await fetch(url)
+        if (!response.ok){
+            throw new Error (`Http error  Status: ${response.status}`);
+
+        }
+
+        const data =await response.json();
+        renderData(data)
+    } catch(err){
+        console.log("There is problem with fetch: " +err)
+    }
 
 }
+getPokemon();
 
-getPokemon()
+function renderData(data){
+    const html = `<h2>${data.name}</h2>
+                    <p>${data.id}</p>
+                    <p> Abilities: ${data.abilities[0].ability.name}, 
+                    ${data.abilities[1].ability.name} </p>`
+        main.insertAdjacentHTML("beforeend", html); 
+}
+
 
